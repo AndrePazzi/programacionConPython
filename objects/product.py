fbe95a61651566488ea375d6847f94ef013f8da1
AndrePazzi <149030295+AndrePazzi@users.noreply.github.com>
2026-05-28 20:02:27 -0700

    Add files via upload

---
+++ objects/product.py
@@ -0,0 +1,7 @@
+class Product:
+    def __init__(self, name, category, price, stock, sales):
+        self.name = name
+        self.category = category
+        self.price = price
+        self.stock = stock
+        self.sales = sales
